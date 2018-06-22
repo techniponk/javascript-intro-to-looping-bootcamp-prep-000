3d22478533c00cf7799ef7c5c015e2c1e4118ca8
Nikki M <nrmonegain@gmail.com>
2018-06-22 23:24:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,5 +17,11 @@ function whileLoop (number) {
 }
 
 function doWhileLoop (array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+  do {
+    array.pop()   
+  } while (array.length > 0 && maybeTrue())
 }
